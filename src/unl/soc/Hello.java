69313261fc14fd77d0972d10f3f5cacb9e520734
jeybb <jeybb@JeyvinWin.unl.edu>
2025-08-28 14:27:57 -0500

    initial commit

---
+++ src/unl/soc/Hello.java
@@ -0,0 +1,17 @@
+package unl.soc;
+
+/**
+ * Author: Jeyvin Coleman
+ * jcoleman24@huskers.unl.edu 
+ * Date: 2025-8-28
+ * 
+ * A simple name and major program in Java
+ * 
+ * 
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("My name is Jeyvin!");
+	}
+}
